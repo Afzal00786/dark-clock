9024802f1ee5ac1b77d13771876435f5d151a86e
Muhammad590 <muhammadsulamanmalik@gmail.com>
2021-12-06 15:58:17 +0500

    navbars

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import { Routes, Route } from "react-router-dom"
 import Home from './Components/Home/home';
 import Navbars from './Components/Navbar/Navbars';
+import Footer from './Components/Footer/Footer';
+import Member from './Components/MemberShip/Member';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/navbar" element={<Navbars />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/member" element={<Member />} />
       </Routes>
 
     </>
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,15 +1,46 @@
-import React from 'react'
-import ""
+import React, { useState } from 'react'
+import layer from "../../Assets/layer.png"
+import twiter from "../../Assets/twiter.png"
+import m from "../../Assets/m.png"
+import git from "../../Assets/git.png"
+import "./Footer.css"
 function Footer() {
+    const [active, setActive] = useState("1")
     return (
         <>
-            <div className="">
+            <div className="Footer_mainDiv">
 
                 <div></div>
+
                 <div>
-                    <h3>All Right reserved Hoopoe Ventures</h3>
+                    <h3 className="footer_text">All Right reserved Hoopoe Ventures</h3>
                 </div>
-                <div></div>
+
+
+                <div className="footer_buttonDiv">
+
+                    <div className={`footer_iconDiv ${active == "1" && "activeBtnss"}`} onClick={() => setActive("1")}>
+                        <img src={layer} />
+                    </div>
+
+                    <div className={`footer_iconDiv ${active == "2" && "activeBtnss"}`} onClick={() => setActive("2")}>
+                        <img src={layer} />
+                    </div>
+
+                    <div className={`footer_iconDiv ${active == "3" && "activeBtnss"}`} onClick={() => setActive("3")}>
+                        <img src={twiter} />
+                    </div>
+
+                    <div className={`footer_iconDiv ${active == "4" && "activeBtnss"}`} onClick={() => setActive("4")}>
+                        <img src={m} />
+                    </div>
+
+                    <div className={`footer_iconDiv ${active == "5" && "activeBtnss"}`} onClick={() => setActive("5")}>
+                        <img src={git} />
+                    </div>
+
+
+                </div>
 
             </div>
         </>
