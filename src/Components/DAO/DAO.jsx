78f65fed87bbda16f37af157ebbc103f92c3f573
sheharyar/10 <sheharyarnaeem521@gmail.com>
2021-12-06 16:00:18 +0500

    my change1

---
+++ src/Components/DAO/DAO.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import "./DAO.css"
+import Navbars from '../Navbar/Navbars'
+
+
+const DAO = () => {
+    return (
+        <>
+        <Navbars />
+        <Container>
+        <div className="dao-section">
+        
+            
+        </div>
+        </Container>
+        </>
+    )
+}
+
+export default DAO
