6d4b83af0369261d1a95843ded5c01c0289a5976
Afzal00786 <afzaltrigonsoft@gmail.com>
2021-12-05 23:22:02 -0800

    crate new assets folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 import { Routes, Route } from "react-router-dom"
+import Home from "./Components/Home/Home.jsx"
 
 function App() {
   return (
