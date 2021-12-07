eafdc88270a8d049b679fe7b663d9be7e96af27a
Afzal00786 <afzaltrigonsoft@gmail.com>
2021-12-07 02:53:38 -0800

    ceate new componet and make responsive

--- src/App.js
+++ src/App.js
@@ -9,18 +9,26 @@ import Navbars from './Components/Navbar/Navbars';
 import Footer from './Components/Footer/Footer';
 import Member from './Components/MemberShip/Member';
 import Stake from './Components/Stake/Stake';
+import UserDashoard1 from "./Components/UserDashoard";
+import UserDashoard2 from "./Components/UserDashoard2";
+import Launchpool from "./Components/Launchpool";
+// import FAQ from "../src/Components/FAQ"
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/userdashoard" element={<UserDashoard1 />} />
+        <Route path="/userdashoard2" element={<UserDashoard2 />} />
+        <Route path="/launchpool" element={<Launchpool />} />
         <Route path="/investment" element={<Investment />} />
         <Route path="/partner" element={<Partner />} />
         <Route path="/navbar" element={<Navbars />} />
         <Route path="/footer" element={<Footer />} />
         <Route path="/member" element={<Member />} />
         <Route path="/stake" element={<Stake />} />
+        {/* <Route path="/faq" element={<FAQ />} /> */}
       </Routes>
 
     </>
